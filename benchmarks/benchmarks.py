a4b09788c805038cfec4be2719552104e15ae2b5
tomas-cabrera <tcabrera@andrew.cmu.edu>
2024-01-06 17:58:07 -0500

    Resolved import in benchmarks/benchmarks.py

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -3,27 +3,14 @@
 For more information on writing benchmarks:
 https://asv.readthedocs.io/en/stable/writing_benchmarks.html."""
 
-import random
-import time
-
-
-def runtime_computation():
-    """Runtime computation consuming between 0 and 5 seconds."""
-
-    time.sleep(random.uniform(0, 5))
-
-
-def memory_computation():
-    """Memory computation for a random list up to 512 samples."""
-
-    return [0] * random.randint(0, 512)
+from thefirstt import example_benchmarks
 
 
 def time_computation():
     """Time computations are prefixed with 'time'."""
-    runtime_computation()
+    example_benchmarks.runtime_computation()
 
 
 def mem_list():
     """Memory computations are prefixed with 'mem' or 'peakmem'."""
-    return memory_computation()
+    return example_benchmarks.memory_computation()
